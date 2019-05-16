20c1fd9a2a9b13022a734c7e0f6ae90b42f85bff
John Ralph Umandal <jrumandal@gmail.com>
2019-05-16 15:49:52 +0200

    Esempio con jquery lista

--- lezione-02-ajax/js/index.js
+++ lezione-02-ajax/js/index.js
@@ -3,22 +3,20 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 ) {
         // this.responseText;
+        const $ul = $('.lista_speciale, [name="aaa"]');
 
-        const elementoHtmlList = document.getElementById('lista');
-
-        const posts = JSON.parse(this.responseText);
+        const postsRaw = JSON.parse(this.responseText);
+        let posts = postsRaw.slice(0, 10);
         for(let i = 0; i < posts.length; i++) {
             /* userId, id, title, body */
             const post = posts[i];
-
-            const elementoDellaLista = document.createElement('li');
-            // elementoDellaLista = <li></li>
-            elementoDellaLista.innerHTML = post.title; // title C
-            // <li>C</li>
-            elementoHtmlList.appendChild(elementoDellaLista);
+            const $li = $('<li></li>');
+            $li.text(post.title);
+            $ul.append($li);
         }
 
     }
 };
 xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
 xhttp.send();
+
