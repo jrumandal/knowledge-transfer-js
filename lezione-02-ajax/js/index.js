919e4a3cc7462e0d8b608079cd15d1ed2c65c345
John Ralph Umandal <jrumandal@gmail.com>
2019-05-16 11:58:35 +0200

    Inserimento del risultato in un elemento HTML lista

--- lezione-02-ajax/js/index.js
+++ lezione-02-ajax/js/index.js
@@ -2,9 +2,21 @@ var xhttp = new XMLHttpRequest();
 // programma l'evento readystatechange
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 ) {
-        const pre = document.getElementById("result-container");
+        // this.responseText;
+        
+        const elementoHtmlList = document.getElementById('lista');
 
-        pre.innerHTML = this.responseText;
+        const posts = JSON.parse(this.responseText);
+        var listaConcatenata = '';
+
+        for(let i = 0; i < posts.length; i++) {
+            /* userId, id, title, body */
+            const post = posts[i];
+
+            listaConcatenata = listaConcatenata + "<li>"+post.title+"</li>";
+        }
+
+        elementoHtmlList.innerHTML = listaConcatenata;
     }
 };
 xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
