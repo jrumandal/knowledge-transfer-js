b508ce345cac1c3b7d4ee15d080e1e4016795c59
John Ralph Umandal <jrumandal@gmail.com>
2019-05-23 12:18:38 +0200

    Aggiunto generatore della griglia

--- montagna/index.js
+++ montagna/index.js
@@ -1,8 +1,37 @@
 /**
- * Costruttore
  * 
+ * @param {HTMLElement} map 
+ * @param {HTMLButtonElement} button 
  */
 function Montagna (map, button) {
     this.btnGenerator = button;
     this.mapContainer = map;
-}
+    this.__SIZE_HEIGHT = 10;
+    this.__SIZE_WIDTH = 10;
+};
+
+Montagna.prototype.init = function init () {
+    this.btnGenerator.addEventListener(
+        'click',
+        () => {
+            this.generateGrid();
+        }
+    );
+};
+
+Montagna.prototype.generateGrid = function generateGrid () {
+    for (let i = 0; i < this.__SIZE_HEIGHT; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        console.log(row); // <div class="row"></div>
+        for(let j = 0; j < this.__SIZE_WIDTH; j++) {
+            const block = document.createElement('div');
+            block.classList.add('block');
+            console.log(block); // <div class="block"></div>
+
+            row.appendChild(block); // <div class="row"><div class="block"></div> .... <div class="block"></div></div>
+        }
+
+        this.mapContainer.appendChild(row);
+    }
+};
